21f59d860efd35f0dd73630697e548e3bf2eb61c
Arjun Naik <naikarjun10@gmail.com>
2025-05-27 10:07:53 +0530

    understanding and writting test case

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,14 +1,53 @@
 import { AppComponent } from "./app.component"
 
+// describe('AppComponent', () => {
+//   let fixture: AppComponent
+
+//   beforeEach(() => {
+//     fixture = new AppComponent()
+//   })
+
+//   it('should have a title Angular-Jest', () => {
+//     expect(fixture.title).toBe('angular-jest-unit-testing')
+//   })
+  
+// })
+
+// describe('addNumbers', () => {
+//   let fixture: AppComponent
+
+//   beforeEach(() => {
+//     fixture = new AppComponent()
+//   })
+
+//   it('should return the sum of num1 and num2', () => {
+//     expect(fixture.addNumbers(10, 20)).toBe(30)
+//   })
+
+//   it('should return a number', () => {
+//     expect(typeof fixture.addNumbers(10, 20)).toBe('number')
+//   })
+// })
+
+// ******** whole of the above code can be combined into a single test suite,   ******** //
 describe('AppComponent', () => {
-  let fixture: AppComponent
+  let fixture: AppComponent;
 
   beforeEach(() => {
-    fixture = new AppComponent()
-  })
+    fixture = new AppComponent();
+  });
 
   it('should have a title Angular-Jest', () => {
-    expect(fixture.title).toBe('angular-jest-unit-testing')
-  })
-  
-})
+    expect(fixture.title).toBe('angular-jest-unit-testing');
+  });
+
+  describe('addNumbers()', () => {
+    it('should return the sum of num1 and num2', () => {
+      expect(fixture.addNumbers(10, 20)).toBe(30);
+    });
+
+    it('should return a number', () => {
+      expect(typeof fixture.addNumbers(10, 20)).toBe('number');
+    });
+  });
+});
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,20 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [FormsModule, RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'angular-jest-unit-testing';
+
+  num1 = 10; // number
+  num2 = 20; // number
+
+  public addNumbers(num1: number, num2: number): number {
+    return num1 + num2; // returns the sum of num1 and num2
+  }
 }
